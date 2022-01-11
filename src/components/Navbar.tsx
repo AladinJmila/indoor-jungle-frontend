38dd76d715dd1b9d7397a2096d2f41a6a1583918
AladinJmila <jmila.aladin@gmail.com>
2022-01-11 14:12:11 +0000

    finished Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,26 @@
+import { Link } from 'react-router-dom';
 import logo from '../assets/jungle-green.png';
 
 const Navbar = () => {
   return (
     <div className='navbar bg-primary'>
-      <div className='navbar-brand text-pink-light-5'>
-        <img className='navbar-logo-wrap' src={logo} alt='jungle' />
-        <h3 className='navbar-text bg-black'>Indoor Jungle</h3>
-      </div>
+      <ul>
+        <li className='navbar-brand text-pink-light-5'>
+          <Link to='/'>
+            <img src={logo} alt='jungle' />
+            <h3 className='navbar-text bg-black'>Indoor Jungle</h3>
+          </Link>
+        </li>
+        <li className='text-white'>
+          <Link to='/login'>Login</Link>
+        </li>
+        <li className='text-white'>
+          <Link to='/signup'>Signup</Link>
+        </li>
+        <li>
+          <button className='btn mr-2'>Logout</button>
+        </li>
+      </ul>
     </div>
   );
 };
