616adc2c2c682224cb0fb0f012cf563da0d80d78
AladinJmila <jmila.aladin@gmail.com>
2022-01-11 16:06:25 +0000

    Added signup and login forms structure

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,19 @@
 const Login = () => {
   return (
-    <div>
+    <form className='auth-form bg-white'>
       <h2>Login</h2>
-    </div>
+      <label>
+        <span>email:</span>
+        <input type='email' />
+      </label>
+      <label>
+        <span>password:</span>
+        <input type='password' />
+      </label>
+      <button className='center-self btn-outlined-secondary bg-hover-secondary text-hover-white '>
+        Login
+      </button>
+    </form>
   );
 };
 
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,8 +1,23 @@
 const Signup = () => {
   return (
-    <div>
-      <h2>Signup</h2>
-    </div>
+    <form className='auth-form bg-white'>
+      <h2>Sign up</h2>
+      <label>
+        <span>email:</span>
+        <input type='email' />
+      </label>
+      <label>
+        <span>password:</span>
+        <input type='password' />
+      </label>
+      <label>
+        <span>diplay name:</span>
+        <input type='text' />
+      </label>
+      <button className='center-self btn-outlined-secondary bg-hover-secondary text-hover-white '>
+        Sign up
+      </button>
+    </form>
   );
 };
 
