3d11014c235431ddd9531dd7cc66118e3571bd21
AladinJmila <jmila.aladin@gmail.com>
2022-01-17 17:31:48 +0000

    First test success, second need work with firebase

--- src/tests/components/PlantCard.test.js
+++ src/tests/components/PlantCard.test.js
@@ -2,6 +2,7 @@ import PlantCard from '../../components/PlantCard';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { timestamp } from './../../firebase/config';
+import { BrowserRouter } from 'react-router-dom';
 
 const photoURL =
   'https://firebasestorage.googleapis.com/v0/b/indoor-jungle.appspot.com/o/plantsPhotos%2FfJ78wu6JJnb1Ga7XwhkIULPaYBB2%2Fmonstera%20adansonii.jpg?alt=media&token=544b1f02-131d-4471-9df1-c30bbb32ffba';
@@ -20,15 +21,28 @@ let getByTestId;
 
 describe('PlantCard Component', () => {
   beforeEach(() => {
-    const component = render(<PlantCard doc={doc} />);
+    const component = render(
+      <BrowserRouter>
+        <PlantCard doc={doc} />
+      </BrowserRouter>
+    );
     getByTestId = component.getByTestId;
   });
 
   describe('WaterDrop Element', () => {
-    it('display 0 if the nextWatering date is today', () => {
+    it('should display 0 if the nextWatering date is today', () => {
       const daysToWaterEl = getByTestId('days-to-water');
 
       expect(daysToWaterEl.textContent).toBe('0');
+    });
+
+    it('should display the watering frequency number as a value if clicked', () => {
+      const waterDropEl = getByTestId('water-drop');
+      const daysToWaterEl = getByTestId('days-to-water');
+
+      fireEvent.click(waterDropEl);
+
+      expect(daysToWaterEl.textContent).toBe('10');
     });
   });
 });
